c603d3034949429f615b10b4411294f5a98143c6
Deborah Thompson <deb246@btinternet.com>
2023-07-17 15:18:24 +0100

    sorted out url for about page

--- recipes/views.py
+++ recipes/views.py
@@ -1,34 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
-recipes =[
-    {
-    'author': 'Me',
-    'title': 'jerk chicken',
-    'directions': 'add spices to chicken, mix, cook in oven',
-    'date_posted': 'July 16th 2023',
-    },
-    {
-    'author': 'Me',
-    'title': 'lemon chicken',
-    'directions': 'add spices to chicken, mix, cook in oven',
-    'date_posted': 'July 19th 2023',
-    },
-    {
-    'author': 'Me',
-    'title': 'apicy chicken',
-    'directions': 'add spices to chicken, mix, cook in oven',
-    'date_posted': 'July 20th 2023',
-    },
-    
+recipes = [
+  {
+    'author': 'Dom V.',
+    'title': 'Meatballs',
+    'content': 'Combine ingredients, form into balls, brown, then place in oven.',
+    'date_posted': 'May 18th, 2022'
+  },
+  {
+    'author': 'Gina R.',
+    'title': 'Chicken Cutlets',
+    'content': 'Bread chicken, cook on each side for 8 min',
+    'date_posted': 'May 18th, 2022'
+  },
+  {
+    'author': 'Bella O.',
+    'title': 'Sub',
+    'content': 'Combine ingredients.',
+    'date_posted': 'May 18th, 2022'
+  }
 ]
 
 # Create your views here.
 def home(request):
-    context = {
-        'recipes': recipes
-    }
-    return render(request, "recipes/home.html", context)
-    
+  context = {
+    'recipes': recipes
+  }
+  return render(request, 'recipes/home.html', context)
 
 def about(request):
-    return render(request, "recipes/about.html", {'title':'about us page'})
+  return render(request, 'recipes/about.html', {'title': 'about page'})
